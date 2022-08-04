176230f0a39fae7857aa3685150aeeeee6a40c04
JK <jmjk@JKs-MacBook-Pro.local>
2022-08-03 18:21:35 -0700

    initial commit with one migration (20220804011009_create_engine_department)

---
+++ db/migrate/20220804011009_create_engine_department.rb
@@ -0,0 +1,11 @@
+class CreateEngineDepartment < ActiveRecord::Migration[6.1]
+  def change
+    create_table :engine_departments do |t|
+      t.string :department_type
+      t.integer :engine_id
+      t.integer :engine_quantity
+      t.string :contact
+    end
+  end
+end
+
