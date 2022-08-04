ae519a0858f71c92a5b092690b44ead8f4194431
JK <jmjk@JKs-MacBook-Pro.local>
2022-08-03 18:32:03 -0700

    plural name for migration 20220804013031_create_engine_departments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_011009) do
+ActiveRecord::Schema.define(version: 2022_08_04_013031) do
 
   create_table "engine_departments", force: :cascade do |t|
     t.string "department_type"
